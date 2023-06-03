b03d5dd2609d76c3688f3d6f5fe62b4f44e0dc7f
William Casarin <jb55@jb55.com>
2023-06-02 18:51:49 -0700

    NostrScript
    NostrScript is a WebAssembly implementation that interacts with Damus.
    It enables dynamic scripting that can be used to power custom list views,
    enabling pluggable algorithms.
    
    The web has JavaScript, Damus has NostrScript. NostrScripts can be
    written in any language that compiles to WASM.
    
    This commit adds a WASM interpreter I've written as a mostly-single C
    file for portability and embeddability. In the future we could
    JIT-compile these for optimal performance if NostrScripts get large and
    complicated. For now an interpreter is simple enough for algorithm list
    view plugins.
    
    Changelog-Added: Add initial NostrScript implementation
    Signed-off-by: William Casarin <jb55@jb55.com>

--- damus/Models/HomeModel.swift
+++ damus/Models/HomeModel.swift
@@ -153,7 +153,7 @@ class HomeModel: ObservableObject {
                 print("nwc: \(resp.req_id) not found in the postbox, nothing to remove [\(relay)]")
             }
             
-            guard let err = resp.response.error else {
+            guard resp.response.error == nil else {
                 print("nwc success: \(resp.response.result.debugDescription) [\(relay)]")
                 nwc_success(state: self.damus_state, resp: resp)
                 return
